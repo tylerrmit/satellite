4da9aa2a24276e8759d294a02dd7b1924a972c72
tylerrmit <54225062+tylerrmit@users.noreply.github.com>
2019-09-17 18:15:19 +1000

    Very basic Jupyter notebook that accepts an address and uses Laura's code to translate it into Latitude/Longitude.
    Next step will be to get it to display the relevant tile for the
    latitude/longitude...  After I've loaded all the phase 2 tiles and built
    all the masks

--- Satellite/Contrib/addressGeocode.py
+++ Satellite/Contrib/addressGeocode.py
@@ -1,22 +1,23 @@
 import requests
-geoCodeUrl = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates"
 
-def addressGeocode(address, geoCodeUrl, outSR = "4326"):
-  #clean up the address for url encoding
-  address = address.replace(" ", "+")
-  address = address.replace(",", "%3B")
+def addressGeocode(address, outSR = "4326"):
+    geoCodeUrl = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates"
+    
+    #clean up the address for url encoding
+    address = address.replace(" ", "+")
+    address = address.replace(",", "%3B")
 
-  #send address to geocode service
-  lookup = requests.get(geoCodeUrl + "?SingleLine=" + address + "&outSR=" + outSR + "&maxLocations=1&f=pjson")
-  data = lookup.json()
+    #send address to geocode service
+    lookup = requests.get(geoCodeUrl + "?SingleLine=" + address + "&outSR=" + outSR + "&maxLocations=1&f=pjson")
+    data = lookup.json()
 
-  if data["candidates"]:
-    #results
-    coords = data["candidates"][0]["location"]
-    return coords
-  else:
-    #no results
-    return "Address not geocoded: " + address
+    if data["candidates"]:
+        #results
+        coords = data["candidates"][0]["location"]
+        return coords
+    else:
+        #no results
+        return "Address not geocoded: " + address
 
-Geocode = addressGeocode("49 Mount Street Eaglemont Vic 3084, Australia", geoCodeUrl)
-print(Geocode)
+#Geocode = addressGeocode("49 Mount Street Eaglemont Vic 3084, Australia")
+#print(Geocode)
